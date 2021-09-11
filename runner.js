0a630e8b6559e97e1c375803aefc4fa35a06373c
Robin Malfait <malfait.robin@gmail.com>
2021-09-11 22:00:53 +0200

    add an example CLI runner
    This will allow you to quickly run:
    
    ```shellsession
    node ./runner.js ./path-to-files.jss
    ```

---
+++ runner.js
@@ -0,0 +1,41 @@
+let fs = require('fs')
+let path = require('path')
+
+let { printer } = require('./src')
+
+let css = require('./examples/css/diagnostics.js')
+let js = require('./examples/javascript/diagnostics.js')
+let html = require('./examples/tailwind/diagnostics.js')
+
+let filePaths = process.argv.slice(2)
+if (filePaths.length <= 0) {
+  console.log('Please provide a file or multiple files to analyze.')
+  process.exit(1)
+}
+filePaths = filePaths.map((file) => path.resolve(file))
+
+let sources = new Map()
+
+let parsers = { js, css, html }
+
+async function run() {
+  let diagnostics = (
+    await Promise.all(
+      filePaths.map(async (file) => {
+        let source = fs.readFileSync(file, 'utf8')
+
+        sources.set(file, source)
+
+        let result = await parsers[path.extname(file).slice(1)](source, { file })
+
+        return result.sort((a, z) => a.loc.row - z.loc.row)
+      })
+    )
+  ).flat(Infinity)
+
+  // ---
+
+  printer(sources, diagnostics, console.log)
+}
+
+run()
