0ba3630cee0d8a43385aca54c5f9bab0a6079d3e
Robin Malfait <malfait.robin@gmail.com>
2022-08-20 22:52:07 +0200

    create different char maps

---
+++ src/printer/char-maps/fancy.js
@@ -0,0 +1,29 @@
+module.exports = {
+  /* ─ */ H: '\u2500',
+  /* │ */ V: '\u2502',
+
+  // Connectors
+  /* ┬ */ TConnector: '\u252C',
+  /* ┤ */ RConnector: '\u2524',
+  /* ┴ */ BConnector: '\u2534',
+  /* ├ */ LConnector: '\u251C',
+
+  // Square corners
+  /* ┌ */ TLSquare: '\u250C',
+  /* ┐ */ TRSquare: '\u2510',
+  /* ┘ */ BRSquare: '\u2518',
+  /* └ */ BLSquare: '\u2514',
+
+  // Rounded corners
+  /* ╭ */ TLRound: '\u256D',
+  /* ╮ */ TRRound: '\u256E',
+  /* ╯ */ BRRound: '\u256F',
+  /* ╰ */ BLRound: '\u2570',
+
+  // Misc
+  /* · */ dot: '\u00B7',
+  /* • */ middot: '\u2022',
+  /* ∙ */ bigdot: '\u2219',
+  /* ■ */ box: '\u25A0',
+  /* ┊ */ VSeparator: '\u250A',
+}
---
+++ src/printer/char-maps/simple.js
@@ -0,0 +1,29 @@
+module.exports = {
+  /* ─ */ H: '-',
+  /* │ */ V: '|',
+
+  // Connectors
+  /* ┬ */ TConnector: '-',
+  /* ┤ */ RConnector: '+',
+  /* ┴ */ BConnector: '+',
+  /* ├ */ LConnector: '+',
+
+  // Square corners
+  /* ┌ */ TLSquare: '+',
+  /* ┐ */ TRSquare: '+',
+  /* ┘ */ BRSquare: '+',
+  /* └ */ BLSquare: '+',
+
+  // Rounded corners
+  /* ╭ */ TLRound: '+',
+  /* ╮ */ TRRound: '+',
+  /* ╯ */ BRRound: '+',
+  /* ╰ */ BLRound: '+',
+
+  // Misc
+  /* · */ dot: '.',
+  /* • */ middot: 'x',
+  /* ∙ */ bigdot: 'x',
+  /* ■ */ box: 'x',
+  /* ┊ */ VSeparator: '|',
+}
