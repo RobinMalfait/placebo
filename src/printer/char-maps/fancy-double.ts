9fefbd723fe4d730d3a34aabd76024381ea9d41d
Robin Malfait <malfait.robin@gmail.com>
2022-08-27 15:58:29 +0200

    add double style
    Becaues uh why not

---
+++ src/printer/char-maps/fancy-double.ts
@@ -0,0 +1,29 @@
+export default {
+  /* ─ */ H: '\u2550',
+  /* │ */ V: '\u2551',
+
+  // Connectors
+  /* ┬ */ TConnector: '\u2566',
+  /* ┤ */ RConnector: '\u2563',
+  /* ┴ */ BConnector: '\u2569',
+  /* ├ */ LConnector: '\u2560',
+
+  // Square corners
+  /* ┌ */ TLSquare: '\u2554',
+  /* ┐ */ TRSquare: '\u2557',
+  /* ┘ */ BRSquare: '\u255D',
+  /* └ */ BLSquare: '\u255A',
+
+  // Rounded corners
+  /* ╭ */ TLRound: '\u2554',
+  /* ╮ */ TRRound: '\u2557',
+  /* ╯ */ BRRound: '\u255D',
+  /* ╰ */ BLRound: '\u255A',
+
+  // Misc
+  /* · */ dot: '\u00B7',
+  /* • */ middot: '\u2022',
+  /* ∙ */ bigdot: '\u2219',
+  /* ■ */ box: '\u25A0',
+  /* ┊ */ VSeparator: '\u250A',
+}
